8dcd59621d62dbbb56d553c54a7c8102d239809f
Potato <artemnovikau@gmail.com>
2026-01-27 14:54:25 +0300

    Add files via upload

--- DZ.py
+++ DZ.py
@@ -15,8 +15,8 @@ class Main_Character:
         print(f"Длина шага: {self.speed}")
 
         print("Скиллы:")
-        for skill in self.skills:
-            skill.status()
+        for i in range(len(self.skills)):
+            self.skills[i].status()
     def UseSkill(self,skill,target):
         if skill in self.skills:
             target.hp -= skill.damage
@@ -40,15 +40,18 @@ class Platform:
         print(f"Цвет: {self.color}")
         print(f"Платформа движется на {self.xChange} по горизонтали и {self.yChange} по вертикали со скоростью {self.velocity}")
 
-class skill:
-    def __init__(self,name,damage,range,mana):
+class Skill:
+    def __init__(self,name,damage,range,mana,special=""):
         self.name = name
         self.range = range
         self.damage = damage
         self.mana = mana
+        self.special = special
     def status(self):
-        print(f"   {self.name}: \n      {self.damage} урона \n      {self.range} радиус атаки \n      {self.mana} маны для каста")
 
+        print(f"   {self.name}: \n      {self.damage} урона \n      {self.range} радиус атаки \n      {self.mana} маны для каста")
+        if self.special !="":
+            print("      "+self.special)
 class Basic_Enemy():
     def __init__(self,name,maxhp,damage,attackspeed,speed):
         self.name = name
@@ -67,7 +70,7 @@ class Basic_Enemy():
     def Step(self,count):
         print(f"{self.name} проходит {count} шагов({count * self.speed} метров)")
     def Attack(self,time, target):
-        print(f"{self.name} атакует{target.name} в течении {time}, бьёт {time//self.ATKspeed} раз и наносит {time//self.ATKspeed*damage} урона")
+        print(f"{self.name} атакует{target.name} в течении {time}, бьёт {time//self.ATKspeed} раз и наносит {time//self.ATKspeed*self.damage} урона")
         self.status()
         target.status()
 class Boss_Enemy():
@@ -96,26 +99,34 @@ class Boss_Enemy():
     def Step(self,count):
         print(f"{self.name} проходит {count} шагов({count * self.speed} метров)")
     def BasicAttack(self,time, target):
-        print(f"{self.name} атакует{target.name} в течении {time}, бьёт {time//self.ATKspeed} раз и наносит {time//self.ATKspeed*damage} урона")
+        print(f"{self.name} атакует{target.name} в течении {time}, бьёт {time//self.ATKspeed} раз и наносит {time//self.ATKspeed*self.damage} урона")
         self.status()
         target.status()
     def UseSkill(self,skill,target):
         if skill in self.skills:
             target.hp -= skill.damage
             self.mana -= skill.mana
-        print(f"{self.name} использует {skill.name} на {target.name}")
-        self.status()
-        target.status()
+            print(f"{self.name} использует {skill.name} на {target.name}")
+            self.status()
+            target.status()
 
-Fireball = skill("Фаербол",15,5,20),
-Burst = skill("Взрыв жопы",30,10,80),
-Savic = skill("Выкрик: САВИЦЕЛИЙ КАНОН",1,20,0),
-Kurs = skill("Выкрик: КУРС ГЕЙ",1,20,0),
-Har = skill("Харакири",9999999999999,-1,-10)
-skills = [Fireball,Burst,Savic,Kurs,Har]
-mc = Main_Character("Стас", 100, skills, 100,2)
-mc.status()
+Fireball = Skill("Фаербол",15,5,20)
+Burst = Skill("Взрыв 5ой точки",30,10,80)
+Savic = Skill("Выкрик: САВИЦЕЛИЙ КАНОН",1,20,0)
+Kurs = Skill("Выкрик: КУРС ГОЛУБЬ",1,20,0)
+Har = Skill("Харакири",9999999999999,-1,-10)
 
+BloodBlade = Skill("кровавый клинок",25,3,30)
+BloodChains = Skill("Кровяная цепь",10,7,25, "Станит на 5 секунд")
+BloodHunt = Skill("Кровяная охота",5,15,75,"Уменьшает урон и скорость передвижения на 20% на 20 секунд")
 
+mc = Main_Character("Стас", 100, [Fireball,Burst,Savic,Kurs,Har], 100,2)
 pl1 = Platform("Красный", 10, 1, 5)
-pl1.status()
+en = Basic_Enemy("Дьявольский миньон", 20, 5,2,1)
+boss = Boss_Enemy("Гига дьявольский миньон", 100, 15,1,2,[BloodBlade,BloodChains,BloodHunt],100)
+
+gameWorld = [pl1, mc, en, boss]
+def ShowGameWorld(gameWorld):
+    for obj in gameWorld:
+        obj.status()
+ShowGameWorld(gameWorld)
